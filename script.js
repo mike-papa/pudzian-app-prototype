297865538dd8d62050d787d6b512986a5a2625b5
mike-papa <mikolajpaziewskialfa@gmail.com>
2022-11-05 14:44:52 +0100

    prawie finalna 2

--- script.js
+++ script.js
@@ -24,6 +24,11 @@ let playNegative2 = document.getElementById("facebook");
 
 // Pudzilla
 let showPudzian = document.getElementById("pudzian");
+let showKufel = document.getElementById("kufel");
+let showPudzianWin = document.getElementById("pudzian_win");
+function showPudzianWinner(){
+  showPudzianWin.style.display = "block";
+}
 
 const mapNegative = new Map();
 mapNegative.set(1,2050);
@@ -53,42 +58,76 @@ mapPositive.set(12,6050);
 
 // positive 1-3
 // negative 4-6
-let rockCounter = 0;
+let  kamien = document.getElementById("kamien")
+
+function updateRock (rockCounter){
+    let stringRockCounter = rockCounter.toString()
+    kamien.src = "images/kamien"+stringRockCounter+".gif";
+}
+let rockCounter = 5;
+updateRock(rockCounter);
 
 function hidePudzian(){
   showPudzian.style.display="none";
 }
+
 function playPositive(){
   let number = Math.floor((Math.random() * 12) + 1);
+  while(number == 9){
+    number = Math.floor((Math.random() * 12) + 1);
+  } 
   number.toString();
   let audio = new Audio("p_"+number.toString()+".mp3"); // dodac sciezke dzwieku
   showPudzian.style.display="block";
-  setTimeout(hidePudzian,mapPositive.get(number));
 //   toggleOff();
 setTimeout(toggleOff,mapPositive.get(number));
 
-  audio.play();
   if(rockCounter >= 0 && rockCounter <= 9){
     rockCounter = rockCounter + 1;
   }
-  if (rockCounter == 10){
-    let winningAudio = new Audio () // Zasluzone piwo
-    winningAudio.play();
+  updateRock(rockCounter);
+  if (rockCounter < 10){
+    setTimeout(hidePudzian,mapPositive.get(number));
+    audio.play();
   }
+
+  if (rockCounter == 10){
+    let winningAudio = new Audio ("browarek.mp3") // Zasluzone piwo
+    showKufel.style.display="block";
+    //showPudzian.src = "images/pudzian_0.png";
+    showPudzian.style.display = "block"; 
+    winningAudio.play();
+    setTimeout(hidePudzian,6050);
+    setTimeout(showPudzianWinner,6051);
+
+  }
+
 }
 
 
 function playNegative(){
   let number = Math.floor((Math.random() * 8) + 1);
+  while (number == 6){
+    number = Math.floor((Math.random() * 8) + 1);
+  }
   let audio = new Audio("n_"+number.toString()+".mp3");
   showPudzian.style.display="block";
-  setTimeout(hidePudzian,mapNegative.get(number));
   //
   setTimeout(toggleOff,mapNegative.get(number));
-  audio.play();
+
   if(rockCounter >= 1 && rockCounter <= 9){
     rockCounter = rockCounter - 1;
   }
+  updateRock(rockCounter);
+  if(rockCounter > 0){
+    setTimeout(hidePudzian,mapNegative.get(number));
+    audio.play();
+  }
+   if(rockCounter == 0){
+    let losingAudio = new Audio ("menel.mp3");
+    setTimeout(hidePudzian,6050);
+    losingAudio.play();
+   }
 }
 
 
@@ -99,3 +138,4 @@ playPositive3.addEventListener("click",playPositive);
 playNegative1.addEventListener("click",playNegative);
 playNegative2.addEventListener("click",playNegative);
 //playNegative3.addEventListener("click",playNegative);
+
